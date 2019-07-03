d6b57b86d89ddf69acafbf318e5132f8e4e9ab25
spiritxhx <spiritxhx@gmail.com>
2019-07-03 12:55:32 -0400

    send the name to server

--- client.js
+++ client.js
@@ -1,14 +1,22 @@
 const net = require('net');
 const connect = () => {
   const conn = net.createConnection({
-    host: '10.0.2.15',
+    host: '192.168.0.102',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
+  conn.on('connect', ()=>{
+    console.log('Successfully connected to game server');
+  });
+
   conn.on('data', data => {
     console.log('Server says: ', data);
+  });
+  
+  conn.on('connect', ()=>{
+    conn.write('Name: XHX');
   });
 
   return conn;
